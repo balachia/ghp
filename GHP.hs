a88bd9533886a2f0807d7c8523b3920cf56085d5
Tony Vashevko <avashevko@gmail.com>
2015-04-10 13:02:34 -0700

    generalizing code

--- GHP.hs
+++ GHP.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE BangPatterns #-}
 
 module GHP where
 
@@ -9,14 +10,22 @@ import Data.HashMap.Strict (keys, unionWith, toList, fromList, fromListWith, emp
 import qualified Data.HashSet as HS
 import Data.Maybe (isJust, fromJust)
 import Data.Foldable (foldl')
+import Data.List (insertBy)
 import Control.Applicative ((<$>), (<*>))
 import Control.Monad (mzero, mapM)
 import Text.Printf (printf)
 import System.Directory (getHomeDirectory)
+import Control.Concurrent
+import Control.Concurrent.STM
+import Control.Concurrent.STM.TBChan
+import Control.Concurrent.STM.TMVar
+import Control.Concurrent.ParallelIO.Local
 import qualified System.IO as IO
 import qualified System.IO.Strict as IOS
 import qualified Data.ByteString.Lazy.Char8 as B
 {-import qualified Data.ByteString.Char8 as B-}
+
+data JSONRes = JSON B.ByteString | Err B.ByteString
 
 data User = User 
     { login :: String
@@ -41,7 +50,11 @@ instance ToJSON User where
 testUserString = B.pack "{\"login\":\"username\", \"id\":1, \"url\":\"foo\", \"type\":\"User\",\"site_admin\":true}"
 
 makePaths :: String -> [Int] -> [String]
-makePaths base range = map (printf base) range
+makePaths base range = map printf' range
+    where
+        {-printf' x = printf (base ++ "/%0.4d/%0.8d.json") (x `quot` 10000) x-}
+        printf' x = printf base (x `quot` 10000) x
+        {-printf' x = printf base x-}
 
 basepath post = do
     home <- getHomeDirectory
@@ -50,36 +63,31 @@ basepath post = do
 
 -- each file contains a bunch of lines
 -- take each line from each file, and bunch them all together
-loadFiles :: [FilePath] -> IO [B.ByteString]
-loadFiles paths = concat <$> (mapM (readLines) paths)
-    where
-        readLines :: FilePath -> IO [B.ByteString]
-        readLines path = (IOS.readFile path) >>= (\x -> return $ B.lines (B.pack x))
-        {-readLines path = (B.readFile path) >>= (\x -> return $ B.lines x)-}
-
-loadFiles' :: [FilePath] -> [IO [B.ByteString]]
-loadFiles' paths = map readLines paths
+loadFiles :: [FilePath] -> [IO [B.ByteString]]
+loadFiles paths = map readLines paths
     where
         readLines path = (IOS.readFile path) >>= (\x -> return $ (tail . B.lines) (B.pack x))
 
 loadFilesWithPath :: [FilePath] -> [IO [(B.ByteString,B.ByteString)]]
 loadFilesWithPath paths = map readLines paths
     where
-        readLines path = (IOS.readFile path) >>= (\x -> return $ [(x, B.pack path) | x <- (tail . B.lines) (B.pack x)])
+        {-readLines path = (IOS.readFile path) >>= (\x -> return $ [(x, B.pack path) | x <- (tail . B.lines) (B.pack x)])-}
+        {-readLines path = readFile' path >>= (\x -> return $ [(x, B.pack path) | x <- (tail . B.lines) (B.pack x)])-}
+        -- is this an arrow?
+        readLines path = (appendPath path) <$> (readFile' path)
+        readFile' path = IOS.readFile path
+        appendPath path x = [(x, B.pack path) | x <- (tail . B.lines) (B.pack x)]
 
--- decode each entry (each line in each file)
-decodeFiles :: FromJSON a => [FilePath] -> IO [Maybe a]
-decodeFiles paths = (map decode) <$> (loadFiles paths)
-
-decodeFiles' :: FromJSON a => [FilePath] -> [IO [Maybe a]]
-{-decodeFiles' :: FromJSON a => [FilePath] -> [IO [Either String a]]-}
-decodeFiles' paths = insertFcn decode (loadFiles' paths)
-{-decodeFiles' paths = insertFcn eitherDecode (loadFiles' paths)-}
+decodeFiles :: FromJSON a => [FilePath] -> [IO [Maybe a]]
+{-decodeFiles :: FromJSON a => [FilePath] -> [IO [Either String a]]-}
+decodeFiles paths = insertFcn decode (loadFiles paths)
+{-decodeFiles paths = insertFcn eitherDecode (loadFiles paths)-}
 
 decodeFilesErr :: FromJSON a => [FilePath] -> [IO [Either B.ByteString a]]
 decodeFilesErr paths = insertFcn decode' (loadFilesWithPath paths)
     where
         decode' :: FromJSON a => (B.ByteString, B.ByteString) -> Either B.ByteString a
+        {-decode' (x,path) = case (decode x) of-}
         decode' (x,path) = case (decode x) of
             Just x' -> Right x'
             Nothing -> Left (B.unlines [path,x])
@@ -101,6 +109,56 @@ writeOutputErr hdl errhdl jsons = map encodeFile jsons
         splitOutput (Right x) = (B.hPutStrLn hdl) $ encode x
         splitOutput (Left x) = (B.hPutStr errhdl x)
 
+writeJSONRes :: IO.Handle -> IO.Handle -> [JSONRes] -> IO ()
+writeJSONRes hdl errhdl jsons = sequence_ $ map writeJSONRes' jsons
+    where
+        writeJSONRes' (JSON x) = B.hPutStrLn hdl x
+        writeJSONRes' (Err x) = B.hPutStrLn errhdl x
+
+-- how to preserve ordering while writing to channel?
+writeOutputTBChan :: ToJSON a => TBChan (Maybe Int, [JSONRes]) -> [IO [Either B.ByteString a]] -> [IO ()]
+writeOutputTBChan chan jsons = zipWith encodeFile jsons [1..]
+    where
+        encodeFile ioxs count = do
+            xs <- ioxs
+            atomically (writeTBChan chan (Just count, (map splitOutput xs)))
+        splitOutput (Right x) = JSON (encode x)
+        splitOutput (Left x) = Err x
+
+-- how to read from channel?
+-- 1) check your state
+--      if first value is terminator, terminate, doing whatever needed to inform parent
+--      if first value is correct, write it out, loop again
+-- 2) read next value from channel, insert it into list, sorted
+--      loop again
+writeFromTBChan :: IO.Handle -> IO.Handle -> TMVar Bool -> TBChan (Maybe Int, [JSONRes]) -> Int -> [(Maybe Int, [JSONRes])] -> IO ()
+--first write with no order preservation
+writeFromTBChan hdl errhdl doneflag chan next state = writeFromTBChan' next state
+    where
+        writeFromTBChan' :: Int -> [(Maybe Int, [JSONRes])] -> IO ()
+        writeFromTBChan' next [] = do
+            item <- atomically (readTBChan chan)
+            case item of
+                (Just i, vals) -> writeFromTBChan' next [item]
+                (Nothing, _) -> endThread
+        writeFromTBChan' next ((Nothing, []):xs) = do
+            endThread
+        writeFromTBChan' next state@((Just i, vals):xs) | i == next = do
+            writeJSONRes hdl errhdl vals
+            writeFromTBChan' (next+1) xs
+                                                 | otherwise = do
+            {-putStrLn (show $ dumpState state)-}
+            item <- atomically (readTBChan chan)
+            case item of
+                (Nothing, _) -> error "Received End-of-Data signal with items left to write"
+                (Just i, vals) -> writeFromTBChan' next (insertBy itemOrder item state)
+        dumpState state = map (\(Just x, _) -> x) state
+        itemOrder (Just x, _) (Just y, _) = compare x y
+        endThread = do
+            atomically $ putTMVar doneflag True
+            putStrLn "Writer Done"
+
+
 -- takes a function f, receiving arguments: intersperse gap, iterations since last intersperse, total iterations
 intersperseIO :: (Int -> Int -> Int -> IO ()) -> Int -> [IO a] -> [IO a]
 intersperseIO f n acts = intersperseIO' f n 1 1 acts
@@ -113,16 +171,6 @@ intersperseIO f n acts = intersperseIO' f n 1 1 acts
         xThenF f n k tot x = x >>= \ret -> ((f n k tot) >> return ret)
 
 traceIO = intersperseIO (\n k tot -> putStrLn (show tot))
-
-{-traceIO :: Int -> [IO a] -> [IO a]-}
-{-traceIO n acts = traceIO' n 1 1 acts-}
-    {-where-}
-        {-traceIO' :: Int -> Int -> Int -> [IO a] -> [IO a]-}
-        {-traceIO' _ _ _ [] = []-}
-        {-traceIO' n k tot (x:[]) = [doThenTrace x tot]-}
-        {-traceIO' n k tot (x:xs) | k == n = (doThenTrace x tot):(traceIO' n 1 (tot+1) xs)-}
-                                {-| otherwise = x:(traceIO' n (k+1) (tot+1) xs)-}
-        {-doThenTrace x tot = x >>= \ret -> ((putStrLn (show tot)) >> return ret)-}
 
 insertFcn :: (a -> b) -> [IO [a]] -> [IO [b]]
 insertFcn f as = map (fmap (map f)) as
@@ -152,6 +200,61 @@ invertKeys kvs = fromListWith (++) [(y,[x]) | (x,y) <- (toList kvs)]
 isSubsetOf :: [Text] -> [Text] -> Bool
 isSubsetOf as bs = HS.null $ (HS.fromList as) `HS.difference` (HS.fromList bs)
 
+-- IO
 
-{-filterOnKeys-}
+processJSONs :: ToJSON a => ([FilePath] -> [IO [Either B.ByteString a]]) -> String -> String -> Int -> IO ()
+processJSONs injsonsf infiles outfile maxFile = do
+    {-inpaths <- basepath infiles-}
+    inpaths <- (++ "/%0.4d/%0.8d.json") <$> (basepath infiles)
+    {-let inpaths = inpaths' ++ "/%0.4d/%0.8d.json"-}
+    let paths = makePaths inpaths [1..maxFile]
+    hdlpath <- basepath (outfile ++ ".json")
+    errhdlpath <- basepath (outfile ++ ".err")
+    hdl <- IO.openFile hdlpath IO.WriteMode
+    errhdl <- IO.openFile errhdlpath IO.WriteMode
+    {-let injsons = (decodeFilesErr f paths)-}
+    outputEach hdl errhdl (injsonsf paths)
+    mapM_ IO.hClose [hdl, errhdl]
+
+parallelProcessJSONs :: ToJSON a => ([FilePath] -> [IO [Either B.ByteString a]]) -> String -> String -> Int -> IO ()
+parallelProcessJSONs injsonsf infiles outfile maxFile = do
+    {-inpaths <- basepath infiles-}
+    inpaths <- (++ "/%0.4d/%0.8d.json") <$> (basepath infiles)
+    {-let inpaths = inpaths' ++ "/%0.4d/%0.8d.json"-}
+    let paths = makePaths inpaths [1..maxFile]
+    hdlpath <- basepath (outfile ++ ".json")
+    errhdlpath <- basepath (outfile ++ ".err")
+    hdl <- IO.openFile hdlpath IO.WriteMode
+    errhdl <- IO.openFile errhdlpath IO.WriteMode
+    {-IO.hSetBuffering hdl (IO.BlockBuffering (Just 10000))-}
+    -- make channels and flags
+    writechan <- atomically $ (newTBChan 10000 :: STM (TBChan (Maybe Int, [JSONRes])))
+    doneflag <- atomically $ (newEmptyTMVar :: STM (TMVar Bool))
+    {-forkIO $ (threadDelay 50000000 >> putStrLn "Starting Writer" >> threadDelay 2000000 >> writeFromTBChan hdl errhdl doneflag writechan 1 [])-}
+    forkIO $ writeFromTBChan hdl errhdl doneflag writechan 1 []
+    {-let injsons = (decodeFilesErr f paths)-}
+    parallelOutputEach 20 writechan (injsonsf paths)
+    {-threadDelay 5000000-}
+    atomically $ writeTBChan writechan (Nothing, [])
+    atomically $ takeTMVar doneflag
+    mapM_ IO.hClose [hdl, errhdl]
+
+-- tracing is an odd notion with parallel processing
+-- maybe move the trace to the writer process?
+parallelOutputEach :: ToJSON a => Int -> TBChan (Maybe Int, [JSONRes]) -> [IO [Either B.ByteString a]] -> IO ()
+parallelOutputEach threads chan injsons = withPool threads (\pool -> parallel_ pool acts)
+    where
+        {-acts = traceIO 500 (writeOutputTBChan chan injsons)-}
+        acts = intersperseIO traceChan 500 (traceIO 500 (writeOutputTBChan chan injsons))
+        traceChan n k tot = do
+            {-free <- atomically $ estimateFreeSlotsTBChan chan-}
+            free <- atomically $ freeSlotsTBChan chan
+            putStrLn ((show free) ++ " free")
+
+outputEach :: ToJSON a => IO.Handle -> IO.Handle -> [IO [Either B.ByteString a]] -> IO ()
+outputEach hdl errhdl injsons = (sequence_ (traceIO 500 (writeOutputErr hdl errhdl injsons)))
+
+outputAll :: ToJSON a => IO.Handle -> IO.Handle -> [IO [Either B.ByteString a]] -> IO ()
+outputAll hdl errhdl injsons = head $ writeOutputErr hdl errhdl [concat <$> (sequence $ traceIO 500 injsons)]
+
 
